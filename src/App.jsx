f177a8b6d129d06f0122dbd2badb89a63a69819e
asifazimi <asifazimi@outlook.com>
2024-07-24 22:01:39 +0500

    Add settings.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <>
-        <h1 className="font-sans text-red-500 font-bold">Hi React</h1>
-      </>
-  )
+      <h1 className="font-sans text-red-500 font-bold">
+        Hi React you are handsom boy
+      </h1>
+    </>
+  );
 }
 
-export default App
+export default App;
