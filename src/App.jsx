c7ac3965f95514ae76cebde269178ab42122043d
asifazimi <asifazimi@outlook.com>
2024-07-25 16:25:07 +0500

    Add Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
   return (
     <>
-      <h1 className="font-sans text-red-500 font-bold">
-        Hi React you are handsom boy
-      </h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          I AM a children
+        </Button>
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
