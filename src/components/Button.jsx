6bfdf020d623734c5d03730027daaeff902a3fa9
asifazimi <asifazimi@outlook.com>
2024-08-06 18:28:19 +0500

    Add HeroImage Animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ children, href, onClick, className, px, white }) => {
-  const classes = `button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
